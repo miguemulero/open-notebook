6f0af6f07e0f855de72930ad6e5d1a32904b25af
miguemulero <migue.mulero@gmail.com>
2026-03-12 13:03:55 +0100

    Add FILEID_MAP for bibliographic file mapping

---
+++ api/bibliografia_map.py
@@ -0,0 +1,5 @@
+FILEID_MAP = {
+    "files/rosales2015": "api/bibliografia/Rosales2015.pdf",
+    "files/perez2019": "api/bibliografia/Perez2019.pdf",
+    "files/fantasiasmuerte2020": "api/bibliografia/FantasiasMuerte2020.pdf",
+}
